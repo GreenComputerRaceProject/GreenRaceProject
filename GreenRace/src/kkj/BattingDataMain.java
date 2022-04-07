8e9930128dd5b4d72aa99e7a1302b92fc42cc48d
82106 <82106@DESKTOP-2HULHF4>
2022-04-07 19:56:08 +0900

    0407/ 경준커밋 / 화면레이아웃추가, 단식 아직 db연동안됨

--- GreenRace/src/kkj/BattingDataMain.java
+++ GreenRace/src/kkj/BattingDataMain.java
@@ -1,12 +1,6 @@
 package kkj;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -14,20 +8,17 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Arrays;
-
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
-import javax.swing.JToggleButton;
 import javax.swing.SwingConstants;
 
-import kkj.BattingDTO;
-
-public class BattingDataMain extends JFrame implements ActionListener {
+public class BattingDataMain {
 	// 프레임, 레이블, 버튼을 만들고 레이블이랑 버튼을 프레임에 붙인다 
 	//jTextField에 값을 입력하고 엔터키를 햇을시 레이블에 값이 반영
+	// ------DAO
 	
 	int[] horse_num = {1,2,3,4,5,6,7,8};
 	ArrayList<String> nickname = new ArrayList<String>();
@@ -40,44 +31,8 @@ public class BattingDataMain extends JFrame implements ActionListener {
 	
 	String sql = null;
 	
-	JButton btn;	
-	JTextField field;
-	JLabel label; 
-	
 	BattingDataMain(){
-		super();
-		Dimension dms = new Dimension(400,100);
-		setLocation(750, 500);
-		setPreferredSize(dms);
-		pack();
-		
-		field = new JTextField();
-		label = new JLabel("배팅금액");
-		label.setHorizontalAlignment(SwingConstants.CENTER);
-		label.setVerticalAlignment(SwingConstants.CENTER);
-		
-		btn = new JButton("확인");
-		btn.addActionListener(this);
-		
-		add(btn, BorderLayout.SOUTH);
-		add(label, BorderLayout.NORTH);
-		add(field, BorderLayout.CENTER);
-		
-		setResizable(false);
-		setVisible(true);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		
-//		for (int i = 0; i < 27; i++) {
-//			GridLayout gl = new GridLayout(3,8,0,0);
-//			setLayout(gl);
-//			JLabel label = new JLabel("1");
-//			add(label);
-//			label.setHorizontalAlignment(JLabel.CENTER);
-//		}
-//			레이블번호를 하나하나바꿀수있는 방법못찾음.. 배당률표에 세팅할라고했음
-		
-//	}
-		
+
 		try {
 
 			Class.forName("org.mariadb.jdbc.Driver");
@@ -103,7 +58,7 @@ public class BattingDataMain extends JFrame implements ActionListener {
 		}finally {
 			close();
 		}
-	//	return data;
+	
 	
 	}
 
@@ -118,13 +73,7 @@ public class BattingDataMain extends JFrame implements ActionListener {
 		new BattingDataMain();
 		
 		
-		
 	}
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		System.out.println(field.getText()); 
-		
-	}
-
 }
+
+
