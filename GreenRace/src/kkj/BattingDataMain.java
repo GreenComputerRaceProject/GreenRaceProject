31c737f511df61b32c6df847b4dfb80668290298
82106 <82106@DESKTOP-2HULHF4>
2022-04-08 10:24:23 +0900

    홀스레이스 메인문 봐주시면돼요

--- GreenRace/src/kkj/BattingDataMain.java
+++ GreenRace/src/kkj/BattingDataMain.java
@@ -20,60 +20,85 @@ public class BattingDataMain {
 	//jTextField에 값을 입력하고 엔터키를 햇을시 레이블에 값이 반영
 	// ------DAO
 	
-	int[] horse_num = {1,2,3,4,5,6,7,8};
-	ArrayList<String> nickname = new ArrayList<String>();
-	ArrayList<Long> gambling_money, tot_money = new ArrayList<Long>();
-	ArrayList<BattingDTO> data = new ArrayList<BattingDTO>();
-	
-	Connection con = null;	
-	Statement stmt = null;	
-	ResultSet rs = null;	
-	
-	String sql = null;
-	
-	BattingDataMain(){
+//	int[] horse_num = {1,2,3,4,5,6,7,8};
+//	ArrayList<String> nickname = new ArrayList<String>();
+//	ArrayList<Long> gambling_money, tot_money = new ArrayList<Long>();
+//	ArrayList<BattingDTO> data = new ArrayList<BattingDTO>();
+//	
+
+	public static void main(String[] args) {
+
 
 		try {
 
 			Class.forName("org.mariadb.jdbc.Driver");
 			
-			con = DriverManager.getConnection("jdbc:mariadb://localhost:3306/race_db", "root", "4546");
+			Connection con = DriverManager.getConnection("jdbc:mariadb://localhost:3306/race_db", "race", "1234");
 			
-			stmt = con.createStatement();
+			Statement stmt = con.createStatement();
 			
 			 ResultSet rs = stmt.executeQuery("select * from dansik");
 			 
-				while(rs.next()) {
-					
-					System.out.println(rs.getString("horse_num"));
-					System.out.println(rs.getString("nickname"));
-					System.out.println(rs.getString("gambling_money"));
-					System.out.println(rs.getString("tot_money"));
-					
-				}
+			 stmt.executeUpdate("insert into dansik " + 
+					 
+   						"(horse_num, nickname, gambling_money, tot_money) " +  "VALUES " + 
+   						
+					 "(4,'파란악마', 800, 4000)");
+			 
+			 while(rs.next()) {
+				 
+				 System.out.println("==============================");
+				 System.out.println("마번 : "+ rs.getString("horse_num"));
+				 System.out.println("유저 닉네임 : "+ rs.getString("nickname"));
+				 System.out.println("유저 배팅금액 : "+ rs.getString("gambling_money"));
+				 System.out.println("전체유저 배팅금액 : "+ rs.getString("tot_money"));
+				 System.out.println("==============================");
+				 
+			 }
+
+				rs.close();
+				stmt.close();
+				con.close();
 				
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}finally {
-			close();
+		
 		}
 	
-	
-	}
-
-	 void close() {
-		 if(rs!=null) try {rs.close();} catch (SQLException e) {}
-		 if(stmt!=null) try {stmt.close();} catch (SQLException e) {}
-		 if(con!=null) try {con.close();} catch (SQLException e) {}		
-	}
-
-	public static void main(String[] args) {
-
-		new BattingDataMain();
-		
-		
 	}
 }
+
+		
+//		try {
+//
+//            Class.forName("org.mariadb.jdbc.Driver");
+//
+//            Connection con = DriverManager.getConnection(
+//
+//            		"jdbc:mariadb://localhost:3306/race_db","race", "1234" );
+//
+//            Statement stmt = con.createStatement();
+//
+//            stmt.executeUpdate("insert into dansik " + 
+//            			
+//                    "(horse_num, nickname, gambling_money, tot_money) " +  "VALUES " + 
+//
+//            		"(,'노란악마', 8000000000, 900000000000)");
+//            
+//            stmt.close();
+//            con.close();
+//
+//        } catch (Exception e) {
+//            // TODO Auto-generated catch block
+//            e.printStackTrace();
+//        }
+//    }
+	
+
+
+
+
+
 
 
