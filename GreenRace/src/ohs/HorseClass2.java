e0f572053dc84e87ce09d645291a23eae37374a4
cyong9573 <cyong9573@naver.com>
2022-04-21 12:00:34 +0900

    0421 말 db 정보 정리

--- GreenRace/src/ohs/HorseClass2.java
+++ GreenRace/src/ohs/HorseClass2.java
@@ -11,43 +11,25 @@ public class HorseClass2 implements Serializable{
 	double speed;
 	double firstspeed;
 	double lastspeed;
-	double stamina;
-	boolean gender;
-	int year;
-	double weight;
 	double state;
-	String recentrecord;
 	
 	
 	public HorseClass2(String hname, String type, String speed, String firstspeed,
-						String lastspeed, String stamina,
-						String gender, String year, String weight, String state, String recentrecord) {
+						String lastspeed,String state) {
 		super();
 		this.hname = hname;
 		this.type = type;
 		this.speed = Double.parseDouble(speed);
 		this.firstspeed = Double.parseDouble(firstspeed);
 		this.lastspeed = Double.parseDouble(lastspeed);
-		this.stamina = Double.parseDouble(stamina);
-		this.gender = Boolean.parseBoolean(gender);
-		this.year = Integer.parseInt(year);
-		this.weight = Double.parseDouble(weight);
 		this.state = Double.parseDouble(state);
-		this.recentrecord = recentrecord;
 	}
 
 
 	@Override
 	public String toString() {
 		return "HorseClass2 [hname=" + hname + ", type=" + type + ", speed=" + speed + ", firstspeed=" + firstspeed
-				+ ", lastspeed=" + lastspeed + ", stamina=" + stamina + ", gender=" + gender + ", year=" + year
-				+ ", weight=" + weight + ", state=" + state + ", recentrecord=" + recentrecord + "]";
+				+ ", lastspeed=" + lastspeed + ", state=" + state + "]";
 	}
-
-
-
-	
-	
-	
 	
 }
